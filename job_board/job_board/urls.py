7d7d8f6306c8429745427dfe0b3005e916afffab
Rodrigo Montelongo <rodrigo00mp@gmail.com>
2025-06-30 12:17:04 -0600

    Added signup/login/logout functionality

--- job_board/job_board/urls.py
+++ job_board/job_board/urls.py
@@ -16,12 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from django.contrib.auth import views
 from core.views import frontpage, signup
 
 urlpatterns = [
   path('', frontpage, name='frontpage'),
   path('signup/', signup, name='signup'),
-    path('admin/', admin.site.urls),
-
+  path('logout/', views.LogoutView.as_view(), name='logout'),
+  path('login/', views.LoginView.as_view(template_name='core/login.html'), name='login'),
+  path('admin/', admin.site.urls),
 ]
