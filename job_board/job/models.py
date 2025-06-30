29cd6841bd8bd6977e6a7a53e0d70457ce73ec54
Rodrigo Montelongo <rodrigo00mp@gmail.com>
2025-06-30 12:26:32 -0600

    Registered job model on admin site

--- job_board/job/models.py
+++ job_board/job/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
-class job(models.Model):
+class Job(models.Model):
   title = models.CharField(max_length=255)
   show_description = models.TextField()
   long_description = models.TextField(blank=True, null=True)
@@ -11,4 +11,6 @@ class job(models.Model):
   created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='jobs')
   created_at = models.DateTimeField(auto_now_add=True)
   changed_at = models.DateTimeField(auto_now=True)
-  
+
+  def __str__(self):
+    return self.title
