737c84bc32ec96d4a55f98f67aed95a41426054f
Rodrigo Montelongo <rodrigo00mp@gmail.com>
2025-06-30 11:44:01 -0600

    Created initial views and templates for frontpage and signup page

--- job_board/job_board/urls.py
+++ job_board/job_board/urls.py
@@ -17,6 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from core.views import frontpage, signup
+
 urlpatterns = [
+  path('', frontpage, name='frontpage'),
+  path('signup/', signup, name='signup'),
     path('admin/', admin.site.urls),
+
 ]
