6702c573c80f7e36c89eac6cf7f8744e12478412
Rodrigo Montelongo <rodrigo00mp@gmail.com>
2025-07-01 12:12:36 -0600

    Created functionality to apply for a job and view applications in dashboard

--- job_board/job/views.py
+++ job_board/job/views.py
@@ -1,13 +1,35 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Job
-from .forms import AddJobForm
+from .forms import AddJobForm, ApplicationForm
 
 # Create your views here.
 def job_detail(request, job_id):
   job = Job.objects.get(pk=job_id)
 
   return render(request, 'job/job_detail.html', {
+    'job': job
+  })
+
+@login_required
+def apply_for_job(request, job_id):
+  job=Job.objects.get(pk=job_id)
+
+  if request.method == 'POST':
+    form = ApplicationForm(request.POST)
+
+    if form.is_valid():
+      application = form.save(commit=False)
+      application.job = job
+      application.created_by = request.user
+
+      application.save()
+      return redirect('dashboard')
+  else:
+    form = ApplicationForm
+  
+  return render(request, 'job/apply_for_job.html', {
+    'form':form,
     'job': job
   })
 
@@ -27,3 +49,4 @@ def add_job(request):
     form  = AddJobForm()
   
   return render(request, 'job/add_job.html', {'form':form})
+
--- job_board/userprofile/views.py
+++ job_board/userprofile/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from job.models import Application
+
 # Create your views here.
 @login_required
 def dashboard(request):
   return render(request, 'userprofile/dashboard.html', {'userprofile':request.user.userprofile})
+
+def view_application(request, application_id):
+  application = Application.objects.get(id=application_id)
+
+  return render(request, 'userprofile/view_application.html',{
+    'application': application
+  })
