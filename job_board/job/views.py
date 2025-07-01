d370cfaea907fca4703acbd83dc379d4afa6af10
Rodrigo Montelongo <rodrigo00mp@gmail.com>
2025-07-01 11:43:04 -0600

    Created form to add job, refactored urls

--- job_board/job/views.py
+++ job_board/job/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from .models import Job
+from .forms import AddJobForm
 
 # Create your views here.
 def job_detail(request, job_id):
@@ -8,3 +10,20 @@ def job_detail(request, job_id):
   return render(request, 'job/job_detail.html', {
     'job': job
   })
+
+@login_required
+def add_job(request):
+  if request.method == 'POST':
+    form = AddJobForm(request.POST)
+
+    if form.is_valid():
+      job = form.save(commit=False)
+      job.created_by = request.user
+      job.save()
+
+      return redirect('dashboard')
+
+  else:
+    form  = AddJobForm()
+  
+  return render(request, 'job/add_job.html', {'form':form})
